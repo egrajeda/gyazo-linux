d87a7227644ebbc2d2b3f51d96e16d43c9937a2d
Eduardo Grajeda <tatofoo@gmail.com>
2010-04-06 15:04:04 -0600

    Added the license to the source file

--- src/gyazolinux.c
+++ src/gyazolinux.c
@@ -1,4 +1,21 @@
-/* TODO: agregar una licencia */
+/*
+Copyright (C) 2010, Eduardo Grajeda <tatofoo@gmail.com>
+
+This file is part of Gyazolinux.
+
+Gyazolinux is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Gyazolinux is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Gyazolinux.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <stdlib.h>
 #include <string.h>
 #include <gtk/gtk.h>
